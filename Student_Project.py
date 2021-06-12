668b6d416498a05305da6d400071b66233b2105d
Pratham2415 <84369631+Pratham2415@users.noreply.github.com>
2021-06-12 18:54:19 +0530

    Add files via upload

---
+++ Student_Project.py
@@ -0,0 +1,15 @@
+import turtle
+
+length = 10#Initialize the length variable
+
+while length <= :#Write the condition
+    for i in range(10):
+        #Write the line for pen color
+        #Write the line for moving the turtle forward by passing length as a variable
+        turtle.right(89)
+        length += 0.5
+        break
+    #Write the line for pen color
+    #Write the line for moving the turtle forward by passing length as a variable
+    turtle.right(89)
+    length += 0.5
